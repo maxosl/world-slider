1d75041f0afd7c483a3ee72e7b02710816a5ee4b
Max Gos <maxosl@gmail.com>
2024-11-11 18:48:22 +0200

    euro centric

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import WorldSlider from './WorldSlider'; // adjust the path if needed
+import DemoMap from './DemoMap';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <main>
-        <WorldSlider />
+        {/* <WorldSlider /> */}
+        <DemoMap />
       </main>
     </div>
   );
